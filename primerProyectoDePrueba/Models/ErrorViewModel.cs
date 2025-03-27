bb02ae17450aad9e10aa35a4065246be2eea877f
laboratorio <laboratorio@UPOAULA10710>
2025-03-27 08:24:09 -0500

    Agregar archivos de proyecto.

---
+++ primerProyectoDePrueba/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace primerProyectoDePrueba.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
